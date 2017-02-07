d4c9a781f2a20049e8f1b666ac8b099a5d535660
MIhail Krivickiy <mkrivickiy@gmail.com>
2017-02-07 16:26:16 +0200

    fixes agencies fields

--- src/Fields/Deleted.php
+++ src/Fields/Deleted.php
@@ -13,4 +13,10 @@ class Deleted extends Field
     protected $custom = false;
     protected $name = 'deleted';
     protected $externalName = 'DeletionMark';
+
+    public function getValue()
+    {
+        return (bool) parent::getValue();
+    }
+
 }
