ccc8a28d914bdda58f6c96a840cca561fc1a5dfb
MIhail Krivickiy <mkrivickiy@gmail.com>
2017-02-07 11:24:22 +0200

    fixes field ID

--- src/Fields/Id.php
+++ src/Fields/Id.php
@@ -12,5 +12,5 @@ class Id extends Field
 {
     protected $custom = false;
     protected $name = 'id';
-    protected $externalName = 'р_ИдентификаторCRM';
+    protected $externalName = 'ИдентификаторCRM';
 }
