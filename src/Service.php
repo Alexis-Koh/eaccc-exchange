4c88da58148acf35f8740fc642d1e048672ea225
MIhail Krivickiy <mkrivickiy@gmail.com>
2017-02-07 09:06:17 +0200

    fixes processed object

--- src/Service.php
+++ src/Service.php
@@ -12,6 +12,8 @@ class Service
 {
 
     protected static $systems = array();
+    /** @var  \MongoCollection $collection */
+    protected static $collection;
 
     protected static  $availableSystems = array(
         'CashBox' => 'Exchange\Systems\CashBox',
@@ -48,6 +50,26 @@ class Service
     protected static function setSystems($systems)
     {
         self::$systems = $systems;
+    }
+
+    /**
+     * @return \MongoCollection
+     */
+    public static function getCollection()
+    {
+        return self::$collection;
+    }
+
+    /**
+     * @param \MongoCollection $collection
+     */
+    public static function setCollection($collection)
+    {
+        self::$collection = $collection;
+    }
+
+    public static function addObject($object) {
+        return self::getCollection()->insert($object);
     }
 
 }
